2936640247cc6167e15f986cf6a140d48cfafdc5
faithcsullivan <faithsullivan179@gmail.com>
2025-03-05 12:05:21 -0500

    movieapp

--- movieapp.py
+++ movieapp.py
@@ -143,7 +143,7 @@ def recommend_similar_movies(movie_title):
     return recommendations
 
 # Streamlit UI
-st.title("❤️‍🔥 If you liked _____, try these!🪼")
+st.title("❤️‍🔥 If you liked _____, try these!")
 
 user_input = st.text_input("Type a movie name to get recommendations:")
 
